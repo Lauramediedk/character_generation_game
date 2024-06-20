c0338b7fc4ff5eba96fc6e2f8edf7996e9bf215a
lauramedie <4700la@gmail.com>
2024-06-20 19:14:25 +0200

    onclick event generating a random character. Created objects and logic

--- app.js
+++ app.js
@@ -35,3 +35,71 @@ const totalTypingTime = typewriterText.length * typingSpeed; //Udregning af saml
 typeWriter("typeText", typewriterText); //typeText er vores html ID og det andet, vores text fra vores const
 showElementAfterDelay("interactive_container", totalTypingTime);
 //Vi kalder vores funktion, og udfylder argumenterne fra vores funktion med vores html id og variablen hvor vi beregner tiden
+
+///////////////////////////////////////////////////////////////////////////////////////
+const wizard = {
+    'name': 'Gavrel the seer',
+    'class': 'Wizard',
+    'Abillities': 'Echantment, Mind manipulation, Fire spells',
+    'History': '...',
+    'imageUrl': 'images/wizard-removebg-preview.png'
+}
+
+const knight = {
+    'name': 'Drystan the defender',
+    'class': 'Knight',
+    'Abillities': 'Bravery, Hope, Heroic',
+    'History': '...',
+    'imageUrl': 'images/knight-removebg-preview.png'
+}
+
+const hunter = {
+    'name': 'Leona the Lonebow',
+    'class': 'Hunter',
+    'Abillities': 'Precision, Loyal to the wilds, Falcons eye',
+    'History': '...',
+    'imageUrl': 'images/hunter-removebg-preview.png'
+}
+
+const characters = [wizard, knight, hunter];
+let lastCharacterIndex = -1;
+//We set an index placement of -1 to begin outside of the randomIndex index calculation
+//And later reassign it. But for now it just works as a starting value in order to compare index placements in the loop later.
+
+function generateCharacter() {
+    let randomIndex;
+    do { //do while loop which executes code at least once, and repeats if condition is true
+        randomIndex = Math.floor(Math.random() * characters.length);
+        //We generate a random character with math random
+        // * it with characters.length in order to be able to randomize between all the array values.
+    } while (randomIndex === lastCharacterIndex);
+    //It runs the loop every time our randomIndex is the same as the last index placement.
+    //So it does the randomIndex, while this "while" condition is true
+
+    lastCharacterIndex = randomIndex; //We reassign the lastCharacterIndex with the newly generated one
+    //from the randomIndex, so we start at -1 to stay out of the index placement at first
+    //But then when the loop runs and a new random index is chosen, we give that to the randomIndex
+    //In order to update it constantly and generate a new character each time.
+
+    const randomCharacter = characters[randomIndex];
+    //We take the array with the values, and get one of the characters, based on our randomIndex
+    //So we first make calculations to generate a random index and then we use that index to get a specific character
+    //That has the index placement in the array.
+    const characterInfo = document.createElement("div");
+    //Dynamically create a div with the info about a character
+    characterInfo.textContent = `Name: ${randomCharacter.name}, Class: ${randomCharacter.class},
+    Abillities: ${randomCharacter.Abillities}`;
+    //Here we use textContent to return the text content of an element.
+    //We simply just add content to the characterInfo, where we use the random generation of a character
+    //And dynamically load that objects/that characters info
+    //We add a , between the abillities
+    const characterImage = document.createElement('img');
+    characterImage.src = randomCharacter.imageUrl;
+    characterImage.alt = randomCharacter.name;
+    //We simply just make a new dynamic element, an img tag and provide it the img and an alt text.
+    const characterContainer = document.getElementById('character_container');
+    characterContainer.innerHTML = ""; //Clear the container to avoid overlapping elements
+    characterContainer.appendChild(characterInfo);
+    characterContainer.appendChild(characterImage);
+    //appendChild appends an element as the last child of an element, meaning we add to the container
+}
